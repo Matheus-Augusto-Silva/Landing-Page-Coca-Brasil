e50ee98ed28ea9ab7fa98936c779c01f92f87a83
Matheus-Augusto-Silva <72157030+Matheus-Augusto-Silva@users.noreply.github.com>
2022-04-28 15:52:05 -0300

    Update swiper.js

--- swiper.js
+++ swiper.js
@@ -1,16 +1,43 @@
-    let swiper = new Swiper('.swiper-container', {
-      effect: 'coverflow',
-      grabCursor: true,
-      centeredSlides: true,
-      slidesPerView: '2',
-      coverflowEffect: {
+//swipper antigo
+//     let swiper = new Swiper('.swiper-container', {
+//       effect: 'coverflow',
+//       grabCursor: true,
+//       centeredSlides: true,
+//       slidesPerView: '2',
+//       coverflowEffect: {
+//         rotate: 0,
+//         stretch: 0,
+//         depth: 100,
+//         modifier: 1,
+//         slideShadows: false,
+//       },
+//       pagination: {
+//         el: '.swiper-pagination',
+//       },
+//     });
+
+var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    speed: 1000,
+    autoplay: {
+        delay: 3000,
+    },
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
         rotate: 0,
-        stretch: 0,
-        depth: 100,
+        stretch: 80,
+        depth: 200,
         modifier: 1,
         slideShadows: false,
-      },
-      pagination: {
-        el: '.swiper-pagination',
-      },
-    });
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+})
